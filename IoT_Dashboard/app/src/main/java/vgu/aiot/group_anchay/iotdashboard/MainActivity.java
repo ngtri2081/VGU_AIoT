49ff8b3e2385c9a8bc8a1acd07553919164edd02
ngtri2081 <16164@student.vgu.edu.vn>
2022-11-03 11:20:40 +0700

    Android App init (Java)

---
+++ IoT_Dashboard/app/src/main/java/vgu/aiot/group_anchay/iotdashboard/MainActivity.java
@@ -0,0 +1,14 @@
+package vgu.aiot.group_anchay.iotdashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
