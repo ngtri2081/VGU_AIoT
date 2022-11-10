90638db8571698a3571771d59262c21e2485c84c
ngtri2081 <16164@student.vgu.edu.vn>
2022-11-10 15:00:54 +0700

    Add Android Configuration

--- IoT_Dashboard/app/src/main/java/vgu/aiot/group_anchay/iotdashboard/MainActivity.java
+++ IoT_Dashboard/app/src/main/java/vgu/aiot/group_anchay/iotdashboard/MainActivity.java
@@ -3,12 +3,61 @@ package vgu.aiot.group_anchay.iotdashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 
 public class MainActivity extends AppCompatActivity {
+    MQTTHelper mqttHelper;
+    TextView txtTemperature, txtHumidity, txtVision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            startMQTT();
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        txtTemperature = findViewById(R.id.textView1);
+        txtHumidity = findViewById(R.id.textView20);
+        txtVision = findViewById(R.id.textView3);
+    }
+
+    public void startMQTT(){
+        mqttHelper = new MQTTHelper(this);
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST", topic + " *** " + message.toString());
+                if (topic.contains("vision-detector")){
+                    txtVision.setText(message.toString());
+                } else if (topic.contains("actuator1")){
+                    txtHumidity.setText(message.toString() + "%");
+                } else if (topic.contains("actuator2")){
+                    txtTemperature.setText(message.toString());
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
     }
 }
